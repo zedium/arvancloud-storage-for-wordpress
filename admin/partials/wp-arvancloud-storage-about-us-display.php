356f0856231ca6bbd4da447082e26a602ec99cb1
Hesam Mohammadi <hesam.m728@gmail.com>
2021-07-17 16:14:57 +0430

    feat: Add about page content

--- admin/partials/wp-arvancloud-storage-about-us-display.php
+++ admin/partials/wp-arvancloud-storage-about-us-display.php
@@ -1,1 +1,9 @@
-About Us Page Coming soon...
+<div class="wrap">
+    <H1><?php echo __( 'About ArvanCloud Storage WordPress Plugin', 'wp-arvancloud-storage' ) ?></H1>
+    <hr>
+    <p>
+        <?php 
+        _e( 'ArvanCloud, an international cloud services provider in Germany, offers various effective solutions for its customers worldwide. As one of the fastest-growing cloud solutions providers in the market, with an exceptional team, cutting-edge technologies and equipment, ArvancCloud aims to become a leader in the cloud industry. <br /> Equipped with a globally distributed network of more than 40 PoPs, ArvanCloud prides itself on innovative cloud solutions at the best price and highest quality. With ArvanCloud solutions, every customer can enjoy a fast, secure, reliable, and affordable cloud service. <br /> ArvanCloud has always provided the broadest set of services possible, to make a significant difference in the cloud services industry. Currently, its portfolio includes numerous cloud products, such as Cloud CDN, Manage DNS, Cloud Security, Video Streaming, Live Streaming Platform, Cloud Object Storage, and Platform as a service.', 'wp-arvancloud-storage' );
+        ?>
+    </p>
+</div>
