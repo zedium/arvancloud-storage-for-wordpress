363f3322b21ffbcc7b1fd17f06d0819b019ded73
Hesam Mohammadi <hesam.m728@gmail.com>
2021-07-18 20:42:34 +0430

    pref: Optimize admin footer file

--- admin/partials/wp-arvancloud-storage-about-us-display.php
+++ admin/partials/wp-arvancloud-storage-about-us-display.php
@@ -6,4 +6,6 @@
         _e( 'ArvanCloud, an international cloud services provider in Germany, offers various effective solutions for its customers worldwide. As one of the fastest-growing cloud solutions providers in the market, with an exceptional team, cutting-edge technologies and equipment, ArvancCloud aims to become a leader in the cloud industry. <br /> Equipped with a globally distributed network of more than 40 PoPs, ArvanCloud prides itself on innovative cloud solutions at the best price and highest quality. With ArvanCloud solutions, every customer can enjoy a fast, secure, reliable, and affordable cloud service. <br /> ArvanCloud has always provided the broadest set of services possible, to make a significant difference in the cloud services industry. Currently, its portfolio includes numerous cloud products, such as Cloud CDN, Manage DNS, Cloud Security, Video Streaming, Live Streaming Platform, Cloud Object Storage, and Platform as a service.', 'wp-arvancloud-storage' );
         ?>
     </p>
+    
+    <?php require_once( ACS_PLUGIN_ROOT . 'admin/partials/wp-arvancloud-storage-footer.php' ); ?>
 </div>
