acb8f0148b896b0263780297bd3bbfe0804ad8f4
Hesam Mohammadi <hesam.m728@gmail.com>
2021-07-24 17:43:35 +0430

    feat: Optimize queries for shared hosts

--- admin/class-wp-arvancloud-storage-admin.php
+++ admin/class-wp-arvancloud-storage-admin.php
@@ -208,6 +208,8 @@ class Wp_Arvancloud_Storage_Admin {
 	public function upload_media_to_storage( $post_id ) {
 
 		if( $bucket_name = get_bucket_name() ) {
+			sleep( 2 ); // Delay execution
+
 			require( ACS_PLUGIN_ROOT . 'includes/wp-arvancloud-storage-s3client.php' );
 
 			$file 	   = is_numeric( $post_id ) ? get_attached_file( $post_id ) : $post_id;
@@ -260,6 +262,9 @@ class Wp_Arvancloud_Storage_Admin {
 
 	// Upload image sub sizes to bucket
 	public function upload_image_to_storage( $args ) {
+
+		sleep( 2 ); // Delay execution
+
 		$upload_dir = wp_upload_dir(); //Get wp upload dir
 		$path 		= str_replace( basename( $args['file'] ), "", $args['file'] );
 		$url	    = $upload_dir['baseurl'] . '/' . $args['file'];
@@ -285,6 +290,8 @@ class Wp_Arvancloud_Storage_Admin {
 	public function delete_media_from_storage( $id ) {
 		
 		if( ( $bucket_name = get_bucket_name() ) && $this->is_attachment_served_by_s3( $id ) ) {
+			sleep( 2 ); // Delay execution
+
 			require( ACS_PLUGIN_ROOT . 'includes/wp-arvancloud-storage-s3client.php' );
 			
 			$client->deleteObject ([
@@ -407,6 +414,8 @@ class Wp_Arvancloud_Storage_Admin {
 
 		if ( $do_action == 'bulk_acs_copy' ) {
 			foreach ( $object_ids as $post_id ) {
+				sleep( 2 ); // Delay execution
+				
 				if( wp_attachment_is_image( $post_id ) ) {
 					$file = wp_get_attachment_metadata($post_id);
 					$this->upload_image_to_storage( $file );
