f7ee768ce0c760f02be6e7de40d5fe2c0d718124
Hesam Mohammadi <hesam.m728@gmail.com>
2021-08-09 23:44:37 +0430

    feat: Change plugin version

--- admin/class-wp-arvancloud-storage-admin.php
+++ admin/class-wp-arvancloud-storage-admin.php
@@ -4,7 +4,7 @@
  * The admin-specific functionality of the plugin.
  *
  * @link       khorshidlab.com
- * @since      1.0.0
+ * @since      0.1.0
  *
  * @package    Wp_Arvancloud_Storage
  * @subpackage Wp_Arvancloud_Storage/admin
